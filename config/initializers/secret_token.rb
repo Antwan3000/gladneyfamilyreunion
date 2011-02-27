4776f8943a67d0209a037c8b28a11f8b76c37246
Antwan Foster <Antwan3000@gmail.com>
2011-02-26 21:27:52 -0500

    Added file structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GladneyreunionOrg::Application.config.secret_token = '0e0b11a9db6375e349a6ef1020c71450a4a59ec12e0d0eac96526b0ea64ab0da69424fd586535a2b8fcdf6ff84d6a047e210e3c5aad5c5c70851a140bbd7c8cb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GladneyreunionOrg::Application.config.session_store :cookie_store, :key => '_gladneyreunion.org_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# GladneyreunionOrg::Application.config.session_store :active_record_store
