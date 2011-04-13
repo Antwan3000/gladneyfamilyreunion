58b22065800d133c1870e7baaca1c58a4a8160a8
Antwan Foster <Antwan3000@gmail.com>
2011-04-13 03:16:20 -0400

    Corrected list size stats to look at key instead of value

--- app/helpers/admin/registrations_helper.rb
+++ app/helpers/admin/registrations_helper.rb
@@ -3,8 +3,8 @@ module Admin::RegistrationsHelper
     line = ""
     Registrant::SHIRTS.each do |k, v|
       unless k == "none"
-        size_count = Registrant.where(:shirt_size => v[:label]).count(:shirt_size)
-        size_count += Guest.where(:shirt_size => v[:label]).count(:shirt_size)
+        size_count = Registrant.where(:shirt_size => k).count(:shirt_size)
+        size_count += Guest.where(:shirt_size => k).count(:shirt_size)
         line += "<li style='float: left; width: 150px; list-style: none;'><span style='margin-right: 20px; padding-right: -10px'>#{v[:alt_label]} = </span><span>#{size_count}</span></li> \n"
       end
     end
